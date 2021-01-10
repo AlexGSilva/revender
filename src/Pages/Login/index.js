9b96472e589e8405853e26ff1a21fc24cfb2ef17
Alexandre <memeregois@gmail.com>
2021-01-10 16:11:46 -0300

    Tela de Login corrigida

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,68 +1,78 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './index.css';
 import { FaUserAlt, FaLock } from 'react-icons/fa';
 import { NavLink } from 'react-router-dom'
 
 import wave from '../../Components/Img/wave.png'
-import bg from '../../Components/Img/bg.svg'
+import logo from '../../Components/Img/logo.svg'
 import avatar from '../../Components/Img/avatar.svg'
 
-const Login = () => {
-  return(
-    <div>
-      <img className="wave" src={wave} alt='Wave'/>
-      <div className='container'>
-        <div className='img'>
-        <img className="bg" src={bg} alt='Bg'/>
-        </div>
-        <div className='login-container'>
-          <form action='index.js'>
-            <img className="avatar" src={avatar} alt='Avatar'/>
-            <h2 className='font-div'>Bem vindo!</h2>
-            <div className='input-div one'>
-              <div className='i'>
-                <i className='icon-user'><FaUserAlt/></i>
-              </div>
-              <div>
+
+class Home extends Component {
+  state = {
+    value: "",
+    hide: false,
+    focus: false,
+  };
+  handleAutoFill = e => {
+    this.setState({
+      hide: e.animationName === "onAutoFillStart",
+    });
+  };
+  render() {
+    const {focusone, focustwo, valueone, valuetwo } = this.state;
+    const hideLabelone =  focusone || valueone;
+    const hideLabeltwo =  focustwo || valuetwo;
+
+    return(
+      <div>
+        <img className="wave" src={wave} alt='Wave'/>
+        <div className='container'>
+          <div className='img'>
+          <img className="logo" src={logo} alt='Logo'/>
+          </div>
+          <div className='login-container'>
+            <form action='index.js'>
+              <img className="avatar" src={avatar} alt='Avatar'/>
+              <h2 className='font-div'>Bem vindo!</h2>
+              <div className={`input-div one ${hideLabelone ? "hide" : ""}`}>
+                <div className='i'>
+                  <i className='icon-user'><FaUserAlt/></i>
+                </div>
+                <div>
                 <h5>Usuário</h5>
-                <input className='input' type='text'></input>
+                  <input 
+                    className='input' 
+                    type='text'
+                    onFocus={() => this.setState({ focusone: true })}
+                    onBlur={() => this.setState({ focusone: false })}
+                    onChange={e => this.setState({ valueone: e.target.value })}
+                    ></input>
+                </div>
               </div>
-            </div>
-            <div className='input-div two'>
-              <div className='i'>
-                <i className='icon-user'><FaLock/></i>
+              <div className={`input-div two ${hideLabeltwo ? "hide" : ""}`}>
+                <div className='i'>
+                  <i className='icon-user'><FaLock/></i>
+                </div>
+                <div>
+                  <h5>Senha</h5>
+                  <input
+                  className='input'
+                  type='password'
+                  onFocus={() => this.setState({ focustwo: true })}
+                  onBlur={() => this.setState({ focustwo: false })}
+                  onChange={e => this.setState({ valuetwo: e.target.value })}
+                  ></input>
+                </div>
               </div>
-              <div>
-                <h5>Senha</h5>
-                <input className='input' type='password'></input>
-              </div>
-            </div>
-            <NavLink to='/home' className='btnforgot'>Não lembra a Senha?</NavLink>
-            <input type='submit' className='btn' value='Login' />
-          </form>
+              <NavLink to='/home' className='btnforgot'>Não lembra a Senha?</NavLink>
+              <input type='submit' className='btn' value='Login' />
+            </form>
+          </div>
         </div>
       </div>
-    </div>
-  )
-}
-
-export default Login;
-
-const inputs = document.querySelectorAll ('.input');
-
-function focusFunc(){
-  let parent = this.parentNode.parentNode;
-  parent.classList.add('focus');
-}
-
-function blurFunc(){
-  let parent = this.parentNode.parentNode;
-  if(this.value === ''){
-    parent.classList.remove('focus');
+    )
   }
 }
 
-inputs.forEach(input => {
-  input.addEventListener('focus', focusFunc);
-  input.addEventListener('blur', blurFunc);
-})
+export default Home;
