ad7dd287473c22e17ce49c2e15aa57f4751e6350
Alexandre <memeregois@gmail.com>
2021-01-10 23:49:51 -0300

    Ajuste Eslint

--- src/Components/Sidebar/sidebar.js
+++ src/Components/Sidebar/sidebar.js
@@ -20,58 +20,58 @@ const Sidebar = ({ sidebarOpen, closeSidebar }) => {
       <div className="sidebar__menu">
         <div className="sidebar__link active_menu_link">
           <i className="fa fa-home"></i>
-          <a href="#">Dashboard</a>
+          <a href="/page404">Dashboard</a>
         </div>
         <h2>MNG</h2>
         <div className="sidebar__link">
           <i className="fa fa-user-secret" aria-hidden="true"></i>
-          <a href="#">Admin Management</a>
+          <a href="/page404">Admin Management</a>
         </div>
         <div className="sidebar__link">
           <i className="fa fa-building-o"></i>
-          <a href="#">Company Management</a>
+          <a href="/page404">Company Management</a>
         </div>
         <div className="sidebar__link">
           <i className="fa fa-wrench"></i>
-          <a href="#">Employee Management</a>
+          <a href="/page404">Employee Management</a>
         </div>
         <div className="sidebar__link">
           <i className="fa fa-archive"></i>
-          <a href="#">Warehouse</a>
+          <a href="/page404">Warehouse</a>
         </div>
         <div className="sidebar__link">
           <i className="fa fa-handshake-o"></i>
-          <a href="#">Contracts</a>
+          <a href="/page404">Contracts</a>
         </div>
         <h2>LEAVE</h2>
         <div className="sidebar__link">
           <i className="fa fa-question"></i>
-          <a href="#">Requests</a>
+          <a href="/page404">Requests</a>
         </div>
         <div className="sidebar__link">
           <i className="fa fa-sign-out"></i>
-          <a href="#">Leave Policy</a>
+          <a href="/page404">Leave Policy</a>
         </div>
         <div className="sidebar__link">
           <i className="fa fa-calendar-check-o"></i>
-          <a href="#">Special Days</a>
+          <a href="/page404">Special Days</a>
         </div>
         <div className="sidebar__link">
           <i className="fa fa-files-o"></i>
-          <a href="#">Apply for leave</a>
+          <a href="/page404">Apply for leave</a>
         </div>
         <h2>PAYROLL</h2>
         <div className="sidebar__link">
           <i className="fa fa-money"></i>
-          <a href="#">Payroll</a>
+          <a href="/page404">Payroll</a>
         </div>
         <div className="sidebar__link">
           <i className="fa fa-briefcase"></i>
-          <a href="#">Paygrade</a>
+          <a href="/page404">Paygrade</a>
         </div>
         <div className="sidebar__logout">
           <i className="fa fa-power-off"></i>
-          <a href="#">Log out</a>
+          <a href="/page404">Log out</a>
         </div>
       </div>
     </div>
